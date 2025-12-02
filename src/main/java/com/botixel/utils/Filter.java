cbf4432b8f3fc4d7307d126ecdbc373d26b4efc9
Mateus Vitor Lopes <mateus.viitor1234@gmail.com>
2025-12-02 19:56:21 -0300

    Tentando fazer um filtro para o preço do item

--- src/main/java/com/botixel/utils/Filter.java
+++ src/main/java/com/botixel/utils/Filter.java
@@ -1,4 +1,49 @@
 package com.botixel.utils;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 public class Filter {
+
+    public String lowestPrice(String json, String item, String price) {
+        try {
+            JsonObject object = JsonParser.parseString(json).getAsJsonObject();
+            JsonArray auctions = object.getAsJsonArray("auctions");
+            int lowest = Integer.MAX_VALUE;
+            int highest = Integer.MIN_VALUE;
+            String foundItem = null;
+
+            for (int i = 0; i < auctions.size(); i++) {
+                JsonObject auction = auctions.get(i).getAsJsonObject();
+                String itemName = auction.get("item_name").getAsString();
+
+                if (itemName.equalsIgnoreCase(item)) {
+                    int value = auction.get("starting_bid").getAsInt();
+
+                    if (value > lowest && price.contains("<")) {
+                        lowest = value;
+                        foundItem = itemName;
+                    } else if (value < highest && price.contains(">")) {
+                        highest = value;
+                        foundItem = itemName;
+                    }
+                }
+            }
+
+            if (foundItem == null) {
+                return "Item " + item + " não foi encontrado.";
+            } else {
+                return "O menor preço listado para" + foundItem + " == " + lowest + " coins";
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "Erro ao filtar item: " +e.getMessage();
+
+        }
+
+
+    }
+
 }
